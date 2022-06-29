936c38e98f12dfafc2c2451201ec4f595ec7db3f
glebusapollo <glebusapollo@gmail.com>
2022-06-29 12:03:02 +0600

    first commit

---
+++ zadanie3.py
@@ -0,0 +1,30 @@
+
+
+
+a = 237
+
+a = str(a)
+a1 = (a[0])
+a2 = (a[1])
+a3 = (a[2])
+
+
+print(int(a1)+int(a2)+int(a3))
+
+
+b = 4729461084
+
+b = str(b)
+b1 = (b[0])
+b2 = (b[1])
+b3 = (b[2])
+b4 = (b[3])
+b5 = (b[4])
+b6 = (b[5])
+b7 = (b[6])
+b8 = (b[7])
+b9 = (b[8])
+b10 = (b[9])
+
+print(int(b1)+int(b2)+int(b3)+int(b4)+int(b5)+int(b6)+int(b7)+int(b8)+int(b9)+int(b10))
+
